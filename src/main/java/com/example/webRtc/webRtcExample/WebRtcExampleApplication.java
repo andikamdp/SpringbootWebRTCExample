2d2d9f43951d5c9538fcc1e8b0feb5e4c41ee927
amd.prapmanto <amd.prapmanto@maybank.co.id>
2022-09-30 16:48:41 +0700

    [Master] init project

---
+++ src/main/java/com/example/webRtc/webRtcExample/WebRtcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.webRtc.webRtcExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRtcExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webRtc/webRtcExample/WebRtcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webRtc.webRtcExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
